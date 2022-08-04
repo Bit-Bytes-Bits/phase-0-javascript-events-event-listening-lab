433a7a0ef7c53afcfa85c5e9d1f4967761565f6a
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-08-04 22:21:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let input;
+
+input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener("click", ()=>{alert('Finally doing what buttons do. You clicked me.')})
 }
+
